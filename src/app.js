18d7c2a19219da8f2abeb0a6e2458482386477aa
Penelope <phuff110@gmail.com>
2023-05-09 13:00:33 -0500

    day 2 wkshp week6 demo

--- src/app.js
+++ src/app.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const app = express();
+const User = require('./models/user.js');
 
-app.use("/static", express.static('public'));
+app.use(express.json());
 
-app.get('/', (request, response) => {
-  response.send(`This is a ${request.method} method`)
+app.get('/users', async (request, response) => {
+  //response.send(`This is a ${request.method} method`)
+  const users = await User.findAll();
+  response.send(users)
+})
+
+app.post('/users', async (req, res, next) => {
+  try{
+    const user = await User.create(req.body)
+    res.send(user.username)
+  } catch (error) {
+    next(error);
+  }
+})
+
+app.get('/users/:username', async (req, res, next) => {
+  try{
+    const user = await User.findOne({
+      where: {username: req.params.username}
+    })
+    res.send(user);
+  }
+  catch(error){
+    next(error)
+  }
 })
 
 module.exports = app;
